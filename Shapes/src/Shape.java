e3ac793f30cca806b20fa8b7f89fae02cfce5251
Xatoka <xatokya@gmail.com>
2016-07-19 13:35:39 +0300

    Square

--- Shapes/src/Shape.java
+++ Shapes/src/Shape.java
@@ -1,4 +1,4 @@
 
 public interface Shape {
- void getPerimeter();
+ double getPerimeter();
 }
---
+++ Shapes/src/Square.java
@@ -0,0 +1,19 @@
+
+public class Square implements Shape {
+    private double side;
+    private final double numberOfSide=4;
+
+    public Square(double side) {
+
+        this.side = side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side*numberOfSide;
+    }
+}
